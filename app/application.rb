477ff3ba1b259520975d1ba8ac1b946f4c517691
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-29 01:30:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,11 +23,7 @@ class Application
     if req.path.match(/cart/)
       if @@cart.empty? 
         resp.write "Your cart is empty"
-      else 
-        @@cart.each do |t| 
-          t 
-        end 
-      end 
+      
       end 
     end 
     
